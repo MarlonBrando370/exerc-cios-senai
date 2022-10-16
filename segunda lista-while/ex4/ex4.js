e453a443082b1bbf2edd9de6cc96b8ded70650f2
MarlonBrando370 <94585871+MarlonBrando370@users.noreply.github.com>
2022-10-16 18:04:12 -0300

    segunda lista criada

---
+++ segunda lista-while/ex4/ex4.js
@@ -0,0 +1,24 @@
+
+
+    
+   // var num1 = Number(prompt('Digite o primeiro número'));
+   // var num2 = Number(prompt('Digite o segundo número'));
+   // var total = n1 + num2;
+   // var confirma = confirm(` A soma dos dois valores: ${total}\n Deseja fazer outra adição?`);
+
+do{
+   
+    var num1 = Number(prompt('Digite o primeiro número'));
+    var num2 = Number(prompt('Digite o segundo número'));
+    var total = num1 + num2;
+    alert(total)
+    var con =confirm('Deseja fazer uma adição?');
+}while(con == true)
+
+      
+    
+     
+
+     
+    
+    
