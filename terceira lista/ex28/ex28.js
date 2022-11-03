ba5a1c8506b601a1c2af4766f8aebda2a60a08e1
MarlonBrando370 <94585871+MarlonBrando370@users.noreply.github.com>
2022-11-03 00:42:18 -0300

    ex28

---
+++ terceira lista/ex28/ex28.js
@@ -0,0 +1,60 @@
+
+    var nem1 =  document.querySelector('input#name1').value
+    
+    var nem2 = document.querySelector('#name2').value ;
+    var nem3 = document.querySelector('#name3').value;
+    var nem4 = document.querySelector('#name4').value;
+    var nem5 = document.querySelector('#name5').value;
+    var nem6 = document.querySelector('#name6').value;
+    var nem7 = document.querySelector('#name7').value;
+    var nem8 = document.querySelector('#name8').value;
+    var nwm9 = document.querySelector('#name9').value;
+    var nem10 = document.querySelector('#name10').value;
+
+    var wage1_input =document.querySelector('input#wage1').value;
+    var wage2_input =document.querySelector('#wage2').value;
+    var wage3_input =document.querySelector('#wage3').value;
+    var wage4_input =document.querySelector('#wage4').value;
+    var wage5_input =document.querySelector('#wage5').value;
+    var wage6_input =document.querySelector('#wage6').value;
+    var wage7_input =document.querySelector('#wage7').value;
+    var wage8_input =document.querySelector('#wage8').value;
+    var wage9_input =document.querySelector('#wage9').value;
+    var wage10_input =document.querySelector('#wage10').value;
+
+    var pNeme1 = document.querySelector('p#p_neme1');
+    var pNeme2 = document.querySelector('#p_neme2');
+    var pNeme1 = document.querySelector('#p_neme3');
+    var pNeme1 = document.querySelector('#p_neme4');
+    var pNeme1 = document.querySelector('#p_neme5');
+    var pNeme1 = document.querySelector('#p_neme6');
+    var pNeme1 = document.querySelector('#p_neme7');
+    var pNeme1 = document.querySelector('#p_neme8');
+    var pNeme1 = document.querySelector('#p_neme9');
+    var pNeme1 = document.querySelector('#p_neme10');
+
+    var pSalAntigo1 = document.querySelector("p#salario_antigo1");
+    var pSalAntigo2 = document.querySelector("#salario_antigo2");
+    var pSalAntigo3 = document.querySelector("#salario_antigo3");
+    var pSalAntigo4 = document.querySelector("#salario_antigo4");
+    var pSalAntigo5 = document.querySelector("#salario_antigo5");
+    var pSalAntigo6 = document.querySelector("#salario_antigo6");
+    var pSalAntigo7 = document.querySelector("#salario_antigo7");
+    var pSalAntigo8 = document.querySelector("#salario_antigo8");
+    var pSalAntigo9 = document.querySelector("#salario_antigo9");
+    var pSalAntigo10 = document.querySelector("#salario_antigo10");
+
+    var pPorcentagem = document.querySelector('p#porcentagem1');
+    var t = document.querySelector('#teste');
+
+    var novoSal = document.querySelector('#salario_novo1');
+     
+   
+    
+    
+    
+    
+    
+
+
+
